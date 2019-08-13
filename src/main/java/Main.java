d98d5a5bdf1c207dca8e743243d208a13b592078
ligovean <ligomail@rambler.ru>
2019-08-13 21:39:54 +0300

    Start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import array.Array;
 import array.ArrayImpl;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +10,7 @@ public class Main {
         Array<Integer> arr2 = new ArrayImpl<Integer>();
         Array<Integer> arr3 = new ArrayImpl<Integer>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10000; i++) {
             int rndInt = (int) (1000*Math.random());
             arr1.add(rndInt);
             arr2.add(rndInt);
@@ -16,8 +18,18 @@ public class Main {
             //Более правильное решение для одинаково заполненных массивов: написать метод для класса ArrayImpl который бы на выход выдавал новый массив идентичный исходному.
         }
 
-        System.out.println("Массив 1: " + arr1.toString());
-        System.out.println("Массив 2: " + arr2.toString());
-        System.out.println("Массив 3: " + arr3.toString());
+        long tStart = TimeUnit.NANOSECONDS.toMillis(System.nanoTime());
+        arr1.sortBubble();
+        System.out.println("Время выполнения сортировки Методом пузырька(мс): " + (TimeUnit.NANOSECONDS.toMillis(System.nanoTime())-tStart));
+        System.out.println();
+
+        tStart = TimeUnit.NANOSECONDS.toMillis(System.nanoTime());
+        arr2.sortSelect();
+        System.out.println("Время выполнения сортировки Методом выбора(мс): " + (TimeUnit.NANOSECONDS.toMillis(System.nanoTime())-tStart));
+        System.out.println();
+
+        tStart = TimeUnit.NANOSECONDS.toMillis(System.nanoTime());
+        arr3.sortInsert();
+        System.out.println("Время выполнения сортировки Методом вставки(мс): " + (TimeUnit.NANOSECONDS.toMillis(System.nanoTime())-tStart));
     }
 }
